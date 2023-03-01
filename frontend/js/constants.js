5df2ef542556cf927b454f12966c7fd6241e3bdc
Joseph Gibri <josephgibri@outlook.com>
2023-03-01 10:53:29 +0200

    goerli test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0xa659683609a823DC261E1B96d73D949ccc8C26A3"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
